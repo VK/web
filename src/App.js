7a011dcbba62e6b20ad36e980c627270419ac59c
VK <viktor@krueckl.de>
2022-03-03 23:07:49 +0100

    tweak

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader'
 
 const SvgShape = ({ shape, color, ...props }) => {
 
-  console.log(shape);
   return (<mesh {...props}>
     <meshBasicMaterial
       color={color}
@@ -23,7 +22,7 @@ const SvgShape = ({ shape, color, ...props }) => {
 function MyFrame({ color, ...props }) {
   const ref = useRef()
   const [r] = useState(() => Math.random() * 10000)
-  useFrame((_) => (ref.current.position.y = -1.75 + Math.sin(_.clock.elapsedTime + r) / 25))
+  useFrame((_) => (ref.current.position.y = -1.75 + Math.sin(_.clock.elapsedTime*0.2 + r) / 40))
   const { paths: [path] } = useLoader(SVGLoader, '/web/frame.svg') // prettier-ignore
   const geom = useMemo(() => SVGLoader.pointsToStroke(path.subPaths[0].getPoints(), path.userData.style), [])
   return (
@@ -40,7 +39,7 @@ function MySvg({ filename, ...config }) {
   const [r] = useState(() => Math.random() * 1000)
   const [sc] = useState(() => 1.5 + Math.random() * .75)
 
-  useFrame((_) => (ref.current.position.y = -1.75 + Math.sin(_.clock.elapsedTime * sc + r) / 35))
+  useFrame((_) => (ref.current.position.y = -1.75 + Math.sin(_.clock.elapsedTime*0.2  * sc + r) /60))
 
   const { paths } = useLoader(SVGLoader, filename)
   const shapes = useMemo(
@@ -50,7 +49,6 @@ function MySvg({ filename, ...config }) {
       ),
     [paths]
   )
-  console.log(shapes);
 
   return (
     <group ref={ref}
@@ -80,30 +78,29 @@ function Rig({ children }) {
 function Ground(props) {
   const [floor, normal] = useTexture(['/web/Ice001_1K_Color.jpg', '/web/Ice001_1K_NormalDX.jpg'])
   return (
-    <Reflector resolution={1024} args={[8, 8]} {...props}>
-      {(Material, props) => <Material color="#f0f0f0" metalness={0} roughnessMap={floor} normalMap={normal} normalScale={[2, 2]} {...props} />}
+    <Reflector resolution={1024} args={[13, 13]} {...props}>
+      {(Material, props) => <Material color="#ccc" metalness={0} roughnessMap={floor} roughness={0.2} normalMap={normal} normalScale={[.2, .2]} {...props} />}
     </Reflector>
   )
 }
 
 export default function App() {
   return (
-    <Canvas dpr={[1, 1.5]} camera={{fov: 110, position: [0, 0, 15] }}>
+    <Canvas dpr={[1, 1.5]} camera={{fov: 115, position: [0, 0, 15] }}>
       <color attach="background" args={['black']} />
-      <ambientLight />
+      <ambientLight intensity={.9} />
       <OrbitControls enableZoom={false} enablePan={false} enableRotate={false} />
       <Suspense fallback={null}>
         <Rig>
           <MyFrame color="#00c2ba" scale={0.008} position={[-1.75, 1.3, -.1]} rotation={[0, 0, 0]} />
 
           <MySvg filename="/web/V.svg" color="#ff26ba" scale={0.0060} position={[-2.2, 1.49, -.12]} rotation={[0, 0.05, -0.01]} />
-          <MySvg filename="/web/K.svg" color="#ff26ba" scale={0.0060} position={[-0.4, 1.4, -0.2]} rotation={[0, -0.1, -.02]} />
-          <MySvg filename="/web/url.svg" color="#ffffff" scale={0.0008} position={[-0.9, 1.0, .8]} rotation={[0, 0, 0]} />
-
-
-          <Ground mirror={1.0} blur={[1000, 100]} mixBlur={12} mixStrength={1.5} rotation={[-Math.PI / 2, 0, Math.PI / 2]} position-y={-0.8} />
+          <MySvg filename="/web/K.svg" color="#ff26ba" scale={0.0060} position={[-0.4, 1.45, -0.2]} rotation={[0, -0.1, -.02]} />
+          <MySvg filename="/web/url.svg" color="#ffffff" scale={0.0008} position={[-0.9, 1.1, 1.2]} rotation={[0, 0, 0]} />
+          <Ground mirror={1.0} blur={[1000, 100]} mixBlur={20} mixStrength={0.7} rotation={[-Math.PI / 2, 0, Math.PI / 2]} position-y={-0.8} />
         </Rig>
         <EffectComposer multisampling={10}>
+          <Bloom kernelSize={1} luminanceThreshold={0} luminanceSmoothing={0.4} intensity={0.7} />
           <Bloom kernelSize={4} luminanceThreshold={0} luminanceSmoothing={0.4} intensity={0.7} />
           <Bloom kernelSize={KernelSize.HUGE} luminanceThreshold={0} luminanceSmoothing={0} intensity={.5} />
         </EffectComposer>
